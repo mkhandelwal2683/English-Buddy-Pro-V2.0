414bcd03d91eff2dfda5a6f272414ff4f0c1bc5d
mkhandelwal2683 <mkhandelwal2683@gmail.com>
2026-07-28 16:31:46 +0530

    Update openrouter-provider.js

--- providers/openrouter-provider.js
+++ providers/openrouter-provider.js
@@ -2,41 +2,26 @@ const OpenRouterProvider = {
 
     async ask(message) {
 
-        const response = await fetch(API_CONFIG.BASE_URL, {
-
-            method: "POST",
-
-            headers: {
-
-                "Authorization": "Bearer " + API_CONFIG.API_KEY,
-
-                "Content-Type": "application/json"
-
-            },
-
-            body: JSON.stringify({
-
-                model: API_CONFIG.MODEL,
-
-                messages: [
-
-                    {
-
-                        role: "user",
-
-                        content: message
-
-                    }
-
-                ]
-
-            })
-
-        });
+        const response = await fetch(
+            "https://sweet-hat-c4cb.mkhandelwal2683.workers.dev/",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    message: message
+                })
+            }
+        );
 
         const data = await response.json();
 
-        return data.choices[0].message.content;
+        if (data.error) {
+            throw new Error(data.error);
+        }
+
+        return data.reply;
 
     }
 
