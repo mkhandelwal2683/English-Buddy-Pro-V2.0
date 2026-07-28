6cd601efd5369fbd2e9c958165944a8ba083404b
mkhandelwal2683 <mkhandelwal2683@gmail.com>
2026-07-28 12:48:35 +0530

    Update provider-manager.js

--- providers/provider-manager.js
+++ providers/provider-manager.js
@@ -1,1 +1,16 @@
 
+/* ==========================================
+   Provider Manager
+========================================== */
+
+const ProviderManager = {
+
+    provider: MockProvider,
+
+    async ask(message) {
+
+        return await this.provider.ask(message);
+
+    }
+
+};
