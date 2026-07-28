2c8c566adbd9c186e698828477dbc6b475e12e17
mkhandelwal2683 <mkhandelwal2683@gmail.com>
2026-07-28 13:06:47 +0530

    Update network-status.js

--- js/network-status.js
+++ js/network-status.js
@@ -1,1 +1,19 @@
+/* ==========================================
+   Network Status Listener
+========================================== */
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    window.addEventListener("online", updateNetwork);
+
+    window.addEventListener("offline", updateNetwork);
+
+    updateNetwork();
+
+});
+
+function updateNetwork() {
+
+    console.log("Network:", Internet.status());
+
+}
