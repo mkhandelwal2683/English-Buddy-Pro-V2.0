ff253aee448b5629e92332aa0cc0d5a186ae6d17
mkhandelwal2683 <mkhandelwal2683@gmail.com>
2026-07-28 14:18:42 +0530

    Update openrouter-provider.js

--- providers/openrouter-provider.js
+++ providers/openrouter-provider.js
@@ -1,1 +1,43 @@
+const OpenRouterProvider = {
 
+    async ask(message) {
+
+        const response = await fetch(API_CONFIG.BASE_URL, {
+
+            method: "POST",
+
+            headers: {
+
+                "Authorization": "Bearer " + API_CONFIG.API_KEY,
+
+                "Content-Type": "application/json"
+
+            },
+
+            body: JSON.stringify({
+
+                model: API_CONFIG.MODEL,
+
+                messages: [
+
+                    {
+
+                        role: "user",
+
+                        content: message
+
+                    }
+
+                ]
+
+            })
+
+        });
+
+        const data = await response.json();
+
+        return data.choices[0].message.content;
+
+    }
+
+};
