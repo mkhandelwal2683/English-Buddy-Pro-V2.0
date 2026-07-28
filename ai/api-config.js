9e48b03d87fc7bf698318970ef214dec2c5961c4
mkhandelwal2683 <mkhandelwal2683@gmail.com>
2026-07-28 14:16:57 +0530

    Update api-config.js

--- ai/api-config.js
+++ ai/api-config.js
@@ -1,1 +1,13 @@
+/* ==========================================
+   API Configuration
+========================================== */
 
+const API_CONFIG = {
+
+    BASE_URL: "https://openrouter.ai/api/v1/chat/completions",
+
+    MODEL: "meta-llama/llama-3.1-8b-instruct:free",
+
+    API_KEY: ""
+
+};
